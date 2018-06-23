155df6a42c9ab423c3d87b737cb43053a102a626
Shai James <skyandbeyond@gmail.com>
2018-06-24 00:18:35 +0300

    comptract readme

--- dev/comptract.sh
+++ dev/comptract.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+# ------------------------------------------
+# This script helps a quicker dev process when writing smart contracts.
+# to use simply navigate to the directory your smart contract is in, and type
+# comptract.sh <ACCOUNT NAME> <Contract Name Which Matches Folder Name>
+# ------------------------------------------
+
+# Example:
+# ------------------------------------------
+# If your contract is called 'hello', name your directory 'hello' as well.
+# cd ~/hello
+# comptract.sh someaccount hello
+# ------------------------------------------
+
+# SET UP:
+# ------------------------------------------
+# cd ~
+# mkdir scripts
+# cd scripts
+# nano comptract.sh
+# --- PASTE IN THIS SCRIPT ---
+# save ( ctrl+x, y, enter )
+# chmod +x comptract.sh
+# --- Add      export PATH=$PATH:~/scripts      to the end of your `~/.profile` ( nano ~/.profile )
+# export PATH=$PATH:~/scripts
+# ------------------------------------------
+
+
+
 if [[ $# -lt 2 ]]; then
     echo "USAGE: comptract.sh <ACCOUNT NAME> <Contract Name> from within the directory"
     exit 1
